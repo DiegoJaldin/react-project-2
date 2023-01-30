c6ae4040012a7481b8d4ebc985511034fe75d4c5
Diego Jaldin <diegoqj99@gmail.com>
2023-01-30 18:58:12 -0400

    react-project-2

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import './App.css'
+import WeatherCard from './components/WeatherCard'
+
+function App() {
+
+  const [coords, setCoords] = useState()
+  const [weather, setWeather] = useState()
+  const [temperature, setTemperature] = useState()
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const success = pos => {
+      const obj = {
+        lat: pos.coords.latitude,
+        lon: pos.coords.longitude
+      }
+      setCoords(obj)
+    }
+    navigator.geolocation.getCurrentPosition(success)
+  }, [])
+
+  useEffect(() => {
+    if(coords){
+      const APIkey = `33e1d85e85dec32e612cced559160f8c`
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${APIkey}`
+
+      axios.get(url)
+        .then(res => {
+          setWeather(res.data)
+          const obj = {
+            celsius: (res.data.main.temp - 273.15).toFixed(1),
+            farenheit: ((res.data.main.temp - 273.15) * 9/5 + 32).toFixed(1)
+          }
+          setTemperature(obj)
+        })
+        .catch(err => console.log(err))
+        .finally(() => setIsLoading(false))
+    }
+  }, [coords])
+
+  
+  return (
+    <div className="App">
+      {
+        isLoading ?
+          <h1>Loading...</h1>
+        :
+          <WeatherCard 
+            weather={weather}
+            temperature={temperature} 
+        />
+      }
+      
+    </div>
+  )
+}
+
+export default App
